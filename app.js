a1a31535692f524a4d3c182efee4a2365c7ba513
Niels Cnops <cnops_niels@hotmail.com>
2023-06-06 12:55:25 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,85 @@
+import express from "express";
+import morgan from "morgan";
+import cors from "cors";
+import { nanoid } from "nanoid";
+
+const app = express();
+app.use(cors());
+
+app.use(express.json());
+app.use(morgan("dev"));
+
+const stuff = [
+  {
+    id: nanoid(),
+    from: "David",
+    text: "handdoeken",
+  },
+  {
+    id: nanoid(),
+    from: "Mehdi",
+    text: "vape",
+  },
+  {
+    id: nanoid(),
+    from: "Mehdi",
+    text: "Badmuts",
+  },
+];
+
+app.post("/stuff", (req, res) => {
+  stuff.push({
+    id: nanoid(),
+    ...req.body,
+  });
+  res.json({ status: "ok" });
+  sendToAllClients();
+});
+
+app.get("/stuff", (req, res) => {
+  res.json(stuff);
+});
+
+const clients = [];
+app.get("/sse", (req, res) => {
+  const headers = {
+    "Content-Type": "text/event-stream",
+    Connection: "keep-alive",
+    "Cache-Control": "no-cache",
+  };
+
+  const client = {
+    time: Date.now(),
+    name: req.query.name,
+  };
+  clients.push({
+    ...client,
+    res: res,
+  });
+
+  const clientsNumber = clients.length;
+
+  res.writeHead(200, headers);
+
+  sendToAllClients();
+
+  req.on("close", () => {
+    console.log(`Connection closed from ${client.name}.`);
+    clients.splice(
+      clients.findIndex((c) => c.time === client.time),
+      1
+    );
+  });
+});
+
+function sendToAllClients() {
+  const clientsNumber = clients.length;
+
+  clients.forEach((c) =>
+    c.res.write(`data: ${JSON.stringify({ stuff, number: clientsNumber })}\n\n`)
+  );
+}
+
+app.listen("1234", function () {
+  console.log("🚀 Server is listening on localhost:1234");
+});
